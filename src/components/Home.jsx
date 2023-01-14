5ab7c2f83fa7db091f832e5bc83583420dbf8bf1
Mohammed A. Abo Sido <abosido2@gmail.com>
2023-01-14 13:54:06 +0200

    Navbar Commponent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar/Navbar";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./style.module.css";
+export default function Navbar() {
+  return (
+    <>
+      <div className={style.wrapper}>
+        <div className={style.left}>
+          <div className={style.name}>Andrew</div>
+          <span>toggle</span>
+        </div>
+        <div className={style.right}>
+          <div className={style.list}>
+            <ul>
+              <li>Home</li>
+              <li>Services</li>
+              <li>Experience</li>
+              <li>Portfolio</li>
+              <li>Testimonials</li>
+            </ul>
+          </div>
+          <button className={`button ${style.nButton}`}>Contact us</button>
+        </div>
+      </div>
+    </>
+  );
+}
